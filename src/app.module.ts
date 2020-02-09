d9df013c904f603869fbb33277bdefec46218380
Mayur Ranjan <mayur.ranjan@4tigo.com>
2020-02-09 13:43:35 +0530

    added connection for db and debugging task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Util } from './util/util';
+
+const mongoURI = (process.env['MONGO_PROD_URL'] || 'mongodb://prod-db:9UuX0WPAmBweFR7zfDuRWJAXntoPRj65ozwh1HPnVaQmCuzwWtXbAYHKjKfBkLjDUwsKNzhU8aRyzvKw8hnPeQ%3D%3D@prod-db.mongo.cosmos.azure.com:10255/sample?ssl=true&replicaSet=globaldb');
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoURI, {
+    connectionName: 'users',
+    retryAttempts: 3
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
