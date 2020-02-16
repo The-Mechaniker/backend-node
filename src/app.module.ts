12184dd027e19bfbfb2f04c17ff04606355c699a
Mayur Ranjan <mayur.ranjan@4tigo.com>
2020-02-16 13:24:40 +0530

    added logger middleware for logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, RequestMethod } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserManagementModule } from './user-management/user-management.module';
 import config from './config/keys';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [
     MongooseModule.forRoot(config.mongoURI, {
       connectionName: 'users',
       retryAttempts: 3,
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectionFactory: connection => {
+        // connection.plugin(require('mongoose-auto-increment'));
+        return connection;
+      },
     }),
     UserManagementModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: import('@nestjs/common').MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
